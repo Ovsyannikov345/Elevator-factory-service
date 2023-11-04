15a34b8098f82df03798ea70fa62ab2da4352f67
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-11-05 00:22:21 +0300

    feat: add order loading from json

--- app/src/pages/OrdersPage.jsx
+++ app/src/pages/OrdersPage.jsx
@@ -1,25 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import OrderList from "../components/OrderList";
 
 const OrdersPage = () => {
-    const [orders, setOrders] = useState([
-        { id: "1", title: "test1", status: "in progress" },
-        { id: "2", title: "test2", status: "complete" },
-        { id: "3", title: "test3", status: "waiting" },
-    ]);
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        loadOrders();
+    }, []);
+
+    const loadOrders = async () => {
+        const response = await fetch("./orders.json");
+        const data = await response.json();
+
+        setOrders(data.orders);
+    };
 
     const showOrderDetails = (id) => {
-        const order = orders.find(order => order.id === id);
+        const order = orders.find((order) => order.id === id);
 
-        console.log(`id: ${order.id} title: ${order.title} status: ${order.status}`);
-    }
+        console.log(
+            `id: ${order.id} title: ${order.title} status: ${order.status}`
+        );
+    };
 
     return (
         <>
             <Header />
             <main>
-                <OrderList orders={orders} detailsHandler={showOrderDetails}/>
+                <OrderList orders={orders} detailsHandler={showOrderDetails} />
             </main>
         </>
     );
