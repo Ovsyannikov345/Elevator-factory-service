1dd2201b2f510322dd4d216bc94c0fb9533f8456
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-11-06 01:43:07 +0300

    feat: add empty orders list information text

--- app/src/components/OrderList.jsx
+++ app/src/components/OrderList.jsx
@@ -4,6 +4,10 @@ import Order from "./Order";
 import classes from "../styles/OrderList.module.css";
 
 const OrderList = ({ orders, detailsHandler, deleteHandler }) => {
+    if (orders.length === 0) {
+        return <h3 className={classes.emptyList}>No orders found</h3>;
+    }
+
     return (
         <div className={classes.orderList}>
             {orders.map((order) => (
